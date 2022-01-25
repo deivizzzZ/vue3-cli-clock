f81b0cbd6dbc4c4cbdef3535c79a60bfecac55c7
David Alvarez <marava.jr@gmail.com>
2022-01-25 17:12:25 +0100

    dayjs added

--- src/components/DigitalClock.vue
+++ src/components/DigitalClock.vue
@@ -1,4 +1,9 @@
 <script>
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+dayjs.extend(utc);
+dayjs.extend(timezone);
 export default {
   name: "DigitalClock",
   props: {
